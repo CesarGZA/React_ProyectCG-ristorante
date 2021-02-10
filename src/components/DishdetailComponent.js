129e3aec34cd0ab55d0f853c13477c20362589df
Cesar Garza <cesarg_00@outlook.com>
2021-02-09 20:20:51 -0600

    Presentational and Container components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,18 +1,16 @@
 import React, {Component} from 'react';
 import {Card, CardImg, CardText,CardBody,CardTitle,List} from 'reactstrap';
 
+
 class DishDetail extends Component{
-    constructor(props){
-        super(props);
-    }
-  
+    
     render(){
-        const renderComments = this.props.dishSelect.comments.map((comment)=>{
+        console.log(this.props.dish.name)
+        const renderComments = this.props.dish.comments.map((comment)=>{
             return(
                 <List type="unstyled">
-                    <li>--{comment.author}, {comment.date}</li>
-                    
                     <li>{comment.comment}</li>
+                    <li>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</li>
                 </List>
 
             );
@@ -23,16 +21,15 @@ class DishDetail extends Component{
                 <div className="row">
                     <div className="col-12 col-md-5 m-1">
                         <Card>
-                            <CardImg  width="100%" src={this.props.dishSelect.image} alt={this.props.dishSelect.name} />
+                            <CardImg  width="100%" src={this.props.dish.image} alt={this.props.dish.name} />
                             <CardBody>
-                                <CardTitle tag="h5">{this.props.dishSelect.name}</CardTitle>
-                                <CardText>{this.props.dishSelect.description}</CardText>
+                                <CardTitle tag="h5">{this.props.dish.name}</CardTitle>
+                                <CardText>{this.props.dish.description}</CardText>
                             </CardBody>
                         </Card>
                     </div>
                     <div className="col-12 col-md-5 m-10">
                         <div><h4>Comments</h4></div>
-                        
                         {renderComments}
                     </div>
                 </div>
