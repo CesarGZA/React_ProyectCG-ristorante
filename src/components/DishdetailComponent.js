23d5566b4caf465e6957385c170e95ed3baeb417
Cesar Garza <cesarg_00@outlook.com>
2021-02-11 00:16:48 -0600

    Header and Footer Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -19,7 +19,6 @@ import {Card, CardImg, CardText,CardBody,CardTitle} from 'reactstrap';
         
     }
     function RenderComments({comments}){
-        
         if(comments != null)
             return(
                 <div className="col-12 col-md-5 m-10">
