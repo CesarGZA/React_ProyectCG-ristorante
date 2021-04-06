f79beda55bc91b48726e58d252b83efcacef1877
Cesar Garza <cesarg_00@outlook.com>
2021-04-05 19:48:58 -0500

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import {Button, Modal, ModalBody, ModalHeader, Label, Row, Col} from "reactstrap";
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 /* ----------------------------------· 3 assigment ------------------------------------------- */
 
@@ -102,7 +103,7 @@ class CommentForm extends Component{
             return( 
                 <div className="col-12 col-md-5 m-1">
                     <Card>
-                        <CardImg  width="100%" src={dish.image} alt={dish.name} />
+                        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
                             <CardTitle tag="h5">{dish.name}</CardTitle>
                             <CardText>{dish.description}</CardText>
